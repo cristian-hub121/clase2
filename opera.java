21a4702e895cdf88d73661f4d31069101f7a614e
cristian-hub121 <cristiansamuelcor@gmail.com>
2025-11-18 09:08:41 -0500

    Create opera.java

---
+++ opera.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pood2.clase2;
+
+import static pood2.clase2.entra.num1;
+import static pood2.clase2.entra.num2;
+
+/**
+ *
+ * @author ALUMNO#
+ */
+public class opera {
+      private int suma;
+
+        public int suma(){
+            
+            suma = num1 + num2;
+            return(suma);
+        }
+    
+    
+}
