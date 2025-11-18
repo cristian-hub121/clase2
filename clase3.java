640beaf788eb303512f5d176e208d4c11dab9a21
cristian-hub121 <cristiansamuelcor@gmail.com>
2025-11-18 09:09:04 -0500

    Create clase3.java

---
+++ clase3.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pood2.clase2;
+
+/**
+ *
+ * @author ALUMNO#
+ */
+public class Clase3 {
+     public static void main(String[] args) {
+         entra en = new entra();
+         opera op = new opera();
+         en.indatos();
+         System.out.println("suma = " +op.suma());
+     }
+}
