f358a46ed8fb7823976ab7f7f111eb5e7dcf6cb2
cristian-hub121 <cristiansamuelcor@gmail.com>
2025-11-18 08:54:45 -0500

    Create operacion.java

---
+++ operacion.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pood2.clase2;
+
+/**
+ *
+ * @author ALUMNO#
+ */
+public class operacion {
+    private int suma;
+    public void ent(){
+        
+    }
+    
+        public int suma(int num1,int num2){
+            
+            suma = num1 + num2;
+            return(suma);
+        }
+    
+}
